d96e7a288194dfe844c3d3f00181863ec7bb1677
Zach Smith <zsmith126028@gmail.com>
2018-06-30 13:43:12 -0500

    Added first problem from the Week 3 hangman game

---
+++ MIT_Python/Week3/wk3p1.py
@@ -0,0 +1,19 @@
+'''
+Please read the Hangman Introduction before starting this problem.
+We'll start by writing 3 simple functions that will help us easily code the Hangman problem.
+First, implement the function isWordGuessed that takes in two parameters -
+a string, secretWord, and a list of letters, lettersGuessed.
+This function returns a boolean - True if secretWord has been guessed
+(ie, all the letters of secretWord are in lettersGuessed) and False otherwise.
+'''
+def is_word_guessed(secret_word, letters_guessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    secret_word_list = list(secret_word)
+    return set(secret_word_list).issubset(letters_guessed)
+
+is_word_guessed('apple',['a', 'e', 'l', 'k', 'p', 'r', 's'])
